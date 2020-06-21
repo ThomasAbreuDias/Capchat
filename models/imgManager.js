71f7459ad3beec367dae342100fdce55529f90d9
ThomasAbreuDias <abreudiasthomas@gmail.com>
2020-06-22 01:31:28 +0200

    Extraction zip

--- models/imgManager.js
+++ models/imgManager.js
@@ -12,9 +12,9 @@ checkCreateFolder = (path) => {
 		      console.log('The uploads folder doesn\'t exist, creating a new one...');
 		      try {
 
-		        fs.mkdir(path, {recursive: true}, (err) => {
-						  if (err) throw err; 
-						});
+		        fs.mkdirSync(path, {recursive: true});
+						//fs.mkdirSync(join(path, 'neutres'));
+						//fs.mkdirSync(join(path, 'singuliers'));
 		        
 		      } catch (err) {
 		        console.log('Error creating the uploads folder 1');
@@ -33,46 +33,11 @@ checkCreateFolder = (path) => {
 						  if (err) throw err; 
 						}));
 */
+
 zipManager = (zip, outputPath) => {
-	/*fs.createReadStream(zip)
-  .pipe(unzipper.Parse())
-  .on('entry', function (entry) {
-    const fileName = entry.path;
-    //const fileName = entry.path.replace(/([' '])/gm, '_');
-    const type = entry.type; // 'Directory' or 'File'
-    const size = entry.vars.uncompressedSize; // There is also compressedSize;
-    console.log(fileName);
-    if (type === 'Directory') {
-      entry.pipe(fs.createWriteStream(outputPath));
-    } else {
-      entry.autodrain();
-    }
-  });*/
-  try {	
-		fs.createReadStream(zip.path)
-	  .pipe(unzipper.Parse())
-	  .pipe(stream.Transform({
-	    objectMode: true,
-	    transform: function(entry,e,cb) {
-	      const fileName = entry.path.replace(/([' '])/gm, '_');
-	      const type = entry.type; // 'Directory' or 'File'
-	      const size = entry.vars.uncompressedSize; // There is also compressedSize;
-	      const path = join(outputPath, fileName);
-	      
-	      if (type === 'Directory') {
-	        entry.pipe(fs.createWriteStream(path))
-	          .on('finish',cb);
-	 
-	      } else {
-	      	console.log(fileName);
-	        entry.autodrain();
-	        cb();
-	      }
-	    }
-	  }));
-	} catch(err){
-	  console.log(err);
-	}
+	console.log(zip);
+	unzipper.Open.file(zip.path)
+  .then(d => d.extract({path: outputPath}));
 }
 
 class imgManager {
@@ -80,11 +45,12 @@ class imgManager {
 	//recuperer le titre et le 
 	static setAssets(capchat, zips) {
 		var op = join(__dirname, '..', 'public', capchat.theme, capchat.titre);//outputPath
-		if(checkCreateFolder(op))
+		//if(checkCreateFolder(op))
 			Object.keys(zips).forEach((key) => {//permet de renomer tous les fichier uploadés pour ajouter le
             let zip = zips[key];
             zipManager(zip, op);
 			});
+		
 	}
 }
 module.exports = imgManager;
